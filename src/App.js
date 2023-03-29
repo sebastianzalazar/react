86bdabd03ff884b0ae4571ec924fb24014511b46
sebastianzalazar <113072897+sebastianzalazar@users.noreply.github.com>
2023-03-28 21:56:22 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Header } from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="create">hola mundo</header>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import  "./header.css"
+
+
+
+
+export function Header(){
+    return <header classname="create">claro</header>
+}
